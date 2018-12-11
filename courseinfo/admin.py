9765bea418d132dcc25bcb06e73738c7a5e0b73f
uuvkk <jl46@illinois.edu>
2018-12-10 19:18:33 -0600

    init

---
+++ courseinfo/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Period)
+admin.site.register(Year)
+admin.site.register(Semester)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Section)
+admin.site.register(Registration)
+
+
